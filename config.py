60d991addefa11b195087182112bf0ea27c20229
djpillen@umich.edu <djpillen@umich.edu>
2016-05-02 12:57:43 -0400

    modify dspace scripts

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ vandura_base_dir = os.path.dirname(os.path.abspath(__file__))
 
 accessions_dir = os.path.join(vandura_base_dir, "ACCESSIONS")
 dspace_mets_dir = os.path.join(vandura_base_dir, "dspace_mets")
+dspace_xoai_dir = os.path.join(vandura_base_dir, "dspace_xoai")
 ead_dir = os.path.join(vandura_base_dir, "EAD")
 marc_dir = os.path.join(vandura_base_dir, "MARC")
 real_masters_all = os.path.join(vandura_base_dir, "Real_Masters_all")
