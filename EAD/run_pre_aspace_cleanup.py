5b2476829e75b74b49a497a888d7fd858fc1f115
djpillen@umich.edu <djpillen@umich.edu>
2016-05-02 11:16:50 -0400

    run cleanup scripts

--- EAD/run_pre_aspace_cleanup.py
+++ EAD/run_pre_aspace_cleanup.py
@@ -26,7 +26,7 @@ def run_pre_aspace_cleanup(ead_dir, dspace_mets_dir):
 	normalize_dates(ead_dir)
 	authfilenumber_urls_to_uris(ead_dir)
 	authfilenumber_propagation(ead_dir)
-	fetch_dspace_mets(ead_dir, dspace_mets_dir)
+	#fetch_dspace_mets(ead_dir, dspace_mets_dir)
 	note_to_odd(ead_dir)
 	move_odds(ead_dir)
 	remove_unitdates_from_ps(ead_dir)
