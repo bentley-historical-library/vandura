7119f8209ea7913fbb68424dd5ea42a4ee9f77b5
djpillen@umich.edu <djpillen@umich.edu>
2016-06-01 11:46:15 -0400

    update athdept, add agent verification script

--- EAD/run_aspace_preliminary_postings.py
+++ EAD/run_aspace_preliminary_postings.py
@@ -13,7 +13,7 @@ from aspace_migration.post_digital_objects import post_digital_objects
 from aspace_migration.update_posted_digital_objects import update_posted_digital_objects
 from aspace_migration.find_missing_refs import find_missing_refs
 
-def run_aspace_preliminary_postings(aspace_ead_dir, subjects_agents_dir, digital_objects_dir, json_dir, resources_dir, migration_stats_dir, dspace_mets_dir, dspace_xoai_dir, aspace_url, username, password):
+def run_aspace_preliminary_postings(aspace_ead_dir, subjects_agents_dir, digital_objects_dir, json_dir, resources_dir, dspace_mets_dir, dspace_xoai_dir, aspace_url, username, password):
 	post_agents_to_aspace(aspace_ead_dir, subjects_agents_dir, aspace_url, username, password)
 	post_subjects(aspace_ead_dir, subjects_agents_dir, aspace_url, username, password)
 	update_posted_subjects(aspace_ead_dir, subjects_agents_dir)
@@ -33,9 +33,8 @@ def main():
 	digital_objects_dir = join(ead_dir, 'digital_objects')
 	json_dir = join(ead_dir, 'json')
 	resources_dir = join(ead_dir, 'resources')
-	migration_stats_dir = join(ead_dir, 'migration_stats')
 	aspace_url, username, password = aspace_credentials()
-	run_aspace_preliminary_postings(aspace_ead_dir, subjects_agents_dir, digital_objects_dir, json_dir, resources_dir, migration_stats_dir, dspace_mets_dir, dspace_xoai_dir, aspace_url, username, password)
+	run_aspace_preliminary_postings(aspace_ead_dir, subjects_agents_dir, digital_objects_dir, json_dir, resources_dir, dspace_mets_dir, dspace_xoai_dir, aspace_url, username, password)
 	print "*** RUN THE RUN_UNITDATE_UNITTITLE_FIX SCRIPT ***"
 
 if __name__ == "__main__":
