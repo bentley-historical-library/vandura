02d4b59c72fd11a96deb3ee9f24a33a9fd22e095
djpillen@umich.edu <djpillen@umich.edu>
2016-06-23 09:29:44 -0400

    incorporate merged muschenheim into real masters all

--- EAD/aspace_prep/copy_eads.py
+++ EAD/aspace_prep/copy_eads.py
@@ -2,12 +2,7 @@ import os
 from os.path import join
 import shutil
 
-def reconcile_muschbas(dst_dir):
-	muschbas = ['muschba.xml', 'muschen.xml']
-	for filename in os.listdir(dst_dir):
-		if filename in muschbas:
-			os.remove(join(dst_dir,filename))
-			print "Removed {0}".format(filename)
+from vandura.config import real_masters_all, ead_dir
 
 def copy_eads(ead_dir, dst_dir):
 	if not os.path.exists(dst_dir):
@@ -20,15 +15,9 @@ def copy_eads(ead_dir, dst_dir):
 		src_file = join(ead_dir,filename)
 		shutil.copy(src_file,dst_dir)
 
-	reconcile_muschbas(dst_dir)
-
 def main():
-	vandura_base_dir = 'C:/Users/djpillen/GitHub/vandura'
-	vandura_real_masters_all = join(vandura_base_dir, 'Real_Masters_all')
-	project_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-	dst_dir = join(project_dir, 'eads')
-	copy_eads(vandura_base_dir,dst_dir)
-	copy_eads(vandura_real_masters_all, dst_dir)
+	dst_dir = join(ead_dir, 'eads')
+	copy_eads(real_masters_all,dst_dir)
 
 if __name__ == "__main__":
 	main()
--- EAD/copy_master_files.py
+++ EAD/copy_master_files.py
@@ -3,7 +3,5 @@ from aspace_prep.copy_eads import copy_eads
 from os.path import join
 
 aspace_ead_dir = join(ead_dir, "eads")
-muschenheim_dir = join(ead_dir, "muschenheim")
 
-copy_eads(muschenheim_dir, aspace_ead_dir)
 copy_eads(real_masters_all, aspace_ead_dir)
